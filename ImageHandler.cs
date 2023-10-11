85d10f32e7be0a3022899a14ef2634642854d0d0
lelt1 <146180783+lelt1@users.noreply.github.com>
2023-10-11 11:41:02 +0100

    Added imageHandler class to handle images being used in FishyNote forms

---
+++ ImageHandler.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Drawing;
+using System.IO;
+
+namespace Fishy_Notes
+{
+    public class ImageHandler
+    {
+        private const String _imgNotFound = @"..\..\image-not-found.png";
+        private Image _image;
+
+        public ImageHandler() 
+        {
+        try
+            {
+                this._image = Bitmap.FromFile(Path.GetFullPath(_imgNotFound));
+            }
+            catch 
+            {
+            this._image = null;
+            }
+        }
+        
+        public Image LoadedImage { get => _image; }
+
+        public ImageHandler(String imgPath)
+        {
+            try
+            {
+                this._image = Bitmap.FromFile(Path.GetFullPath(imgPath));
+            }
+            catch
+            {
+                throw new InvalidStringException("ImageHandler: Invalid path / filename");
+            }
+        }
+    }
+}
